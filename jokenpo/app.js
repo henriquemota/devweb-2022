95087ebff64327519328af4f19f800c3e4363b36
henriquemota <mota.henrique@gmail.com>
2022-05-19 20:21:06 -0300

    implementacao correta do jogo

---
+++ jokenpo/app.js
@@ -0,0 +1,17 @@
+const play = (playerMovment) => {
+	const pre = document.getElementById('response')
+	const movements = ['pedra', 'papel', 'tesoura']
+	const sorted = movements[Math.floor(Math.random() * movements.length)]
+
+	let resultado = null
+	if (playerMovment === sorted) resultado = 'empate'
+	else if (
+		(playerMovment === 'pedra' && sorted !== 'papel') ||
+		(playerMovment === 'papel' && sorted !== 'tesoura') ||
+		(playerMovment === 'tesoura' && sorted !== 'pedra')
+	)
+		resultado = 'você venceu'
+	else resultado = 'app venceu'
+
+	pre.innerHTML = `<p>computador jogou ${sorted}, ${resultado}</p>`
+}
