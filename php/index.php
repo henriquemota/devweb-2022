1bf4bb7cd82088731f89ea0ab7fe72db482fb9d1
henriquemota <mota.henrique@gmail.com>
2022-05-26 19:09:11 -0300

    implementacoes da parte do PHP

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Primeira página em php</h1>
+  a soma de 1 + 1 é <?echo  1 + 1; ?>
+</body>
+</html>
